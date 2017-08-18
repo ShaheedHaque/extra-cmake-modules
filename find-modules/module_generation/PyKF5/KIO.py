bcc378ad72c18195412fa06892234e47cb857738
Shaheed Haque <srhaque@theiet.org>
2017-08-18 11:56:29 +0100

    Use generic array support.

--- find-modules/module_generation/PyKF5/KIO.py
+++ find-modules/module_generation/PyKF5/KIO.py
@@ -22,7 +22,6 @@ PyKF5.KIOGui PyKF5.KIOWidgets PyKF5.kio. This modules describes:
 
     * Supplementary SIP file generator rules.
 """
-
 from clang.cindex import TokenKind, TypeKind
 
 import rule_helpers
@@ -43,142 +42,16 @@ def _function_rewrite_using_decl2(container, function, sip, matcher):
     sip["fn_result"] = "virtual void"
 
 
-def variable_rewrite_array(container, variable, sip, matcher):
-    base_type, indices = sip["decl"].split("[", 1)
-    indices = "[" + indices
-    #
-    # If all the indices are empty, as in [][][], then just convert to ***.
-    #
-    if indices == "[]":
-        sip["decl"] = base_type + "*"
-        return
-    #
-    # We only handle simple 1D arrays of byte values.
-    #
-    if variable.type.kind != TypeKind.CONSTANTARRAY:
-        return
-    element_type = variable.type.element_type.get_canonical()
-    if element_type.kind in [TypeKind.CHAR_S, TypeKind.CHAR_U, TypeKind.SCHAR, TypeKind.UCHAR]:
-        sip["decl"] = "SIP_PYBUFFER"
-        code = """
-{
-%GetCode
-    char *cxxvalue = (char *)&sipCpp->{name}[0];
-
-    // Create the Python buffer.
-    Py_ssize_t element_count = {element_count};
-    sipPy = PyByteArray_FromStringAndSize(cxxvalue, element_count);
-%End
-
-%SetCode
-    char *cxxvalue = (char *)&sipCpp->{name}[0];
-    Py_ssize_t element_count = {element_count};
-    const char *name = "{name}";
-
-    if (!PyByteArray_Check(sipPy)) {
-        PyErr_Format(PyExc_TypeError, "expected buffer");
-        sipErr = 1;
-    }
-
-    // Convert the buffer to C++.
-    if (!sipErr) {
-        if (PyByteArray_GET_SIZE(sipPy) != element_count) {
-            PyErr_Format(PyExc_ValueError, "'%s' must have length %ld", name, element_count);
-            sipErr = 1;
-        } else {
-            memcpy(cxxvalue, PyByteArray_AsString(sipPy), element_count);
-        }
-    }
-%End
-}"""
-    elif element_type.kind in [TypeKind.USHORT, TypeKind.UINT, TypeKind.ULONG, TypeKind.ULONGLONG, TypeKind.UINT128,
-                               TypeKind.SHORT, TypeKind.INT, TypeKind.LONG, TypeKind.LONGLONG, TypeKind.INT128,
-                               TypeKind.ENUM]:
-        sip["decl"] = base_type + "*"
-        sip["decl"] = "SIP_PYTUPLE"
-        code = """
-{
-%GetCode
-    typedef {cxx_t} CxxvalueT;
-    CxxvalueT *cxxvalue = (CxxvalueT *)&sipCpp->{name}[0];
-    Py_ssize_t element_count = {element_count};
-    int sipErr = 0;
-
-    // Create the Python tuple.
-    PyObject *tuple = PyTuple_New(element_count);
-    if (!tuple) {
-        PyErr_Format(PyExc_TypeError, "unable to create a tuple");
-        sipErr = 1;
-    }
-
-    // Populate the tuple elements.
-    if (!sipErr) {
-        Py_ssize_t i = 0;
-        for (i = 0; i < element_count; ++i) {
-#if PY_MAJOR_VERSION >= 3
-            PyObject *value = PyLong_FromLong((long)cxxvalue[i]);
-#else
-            PyObject *value = PyInt_FromLong((long)cxxvalue[i]);
-#endif
-            if (value == NULL) {
-                PyErr_Format(PyExc_TypeError, "cannot insert value into tuple");
-                Py_XDECREF(value);
-                Py_DECREF(tuple);
-                sipErr = 1;
-            } else {
-                PyTuple_SET_ITEM(tuple, i, value);
-            }
-        }
-    }
-    sipPy = sipErr ? tuple : NULL;
-%End
-
-%SetCode
-    typedef {cxx_t} CxxvalueT;
-    CxxvalueT *cxxvalue = (CxxvalueT *)&sipCpp->{name}[0];
-    Py_ssize_t element_count = {element_count};
-    const char *name = "{name}";
-
-    if (!PyTuple_Check(sipPy)) {
-        PyErr_Format(PyExc_TypeError, "expected tuple");
-        sipErr = 1;
-    }
-
-    // Convert the tuple to C++.
-    if (!sipErr) {
-        if (PyTuple_GET_SIZE(sipPy) != element_count) {
-            PyErr_Format(PyExc_ValueError, "'%s' must have length %ld", name, element_count);
-            sipErr = 1;
-        } else {
-            Py_ssize_t i = 0;
-            for (i = 0; i < element_count; ++i) {
-                PyObject *value = PyTuple_GetItem(sipPy, i);
-#if PY_MAJOR_VERSION >= 3
-                cxxvalue[i] = (CxxvalueT)PyLong_AsLong(value);
-#else
-                cxxvalue[i] = (CxxvalueT)PyInt_AsLong(value);
-#endif
-            }
-        }
-    }
-%End
-}"""
-    else:
-        return
-    code = code.replace("{cxx_t}", element_type.spelling)
-    code = code.replace("{element_count}", str(variable.type.element_count))
-    code = code.replace("{name}", sip["name"])
-    sip["code"] = code
-
-
 def variable_fully_qualify(container, variable, sip, matcher):
     sip["decl"] = "KNTLM::" + sip["decl"]
+
 
 def module_fix_KIOCore_kio(filename, sip, rule):
     rule_helpers.module_delete_imports(filename, sip, rule, "KIOCore/KIO/KIOmod.sip")
     rule_helpers.modulecode_delete(filename, sip, rule, "QMap<QString, QString>")
     rule_helpers.module_add_classes(filename, sip, rule, "KIO::JobUiDelegateExtension /External/",
                                     "KIO::MetaData /External/")
+
 
 def module_fix_KIOCore_KIO(filename, sip, rule):
     rule_helpers.module_delete_imports(filename, sip, rule, "KIOCore/KIOCoremod.sip")
@@ -300,10 +173,6 @@ def typedef_rules():
 
 def variable_rules():
     return [
-        #
-        # Emit code for fixed arrays.
-        #
-        ["KNTLM::.*", ".*", ".*\[.+\]", variable_rewrite_array],
         #
         # Fully-qualify.
         #
