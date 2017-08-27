507645d5bbb84e8be8d9eaaa78a497bbaea0d423
Shaheed Haque <srhaque@theiet.org>
2017-08-27 14:02:08 +0100

    Get KIOWidgets compiling.

--- find-modules/module_generation/PyKF5/KIOWidgets.py
+++ find-modules/module_generation/PyKF5/KIOWidgets.py
@@ -21,22 +21,33 @@ SIP binding customisation for PyKF5.KIOWidgets. This modules describes:
 
     * Supplementary SIP file generator rules.
 """
-
 import rule_helpers
 
 
-def module_fix_mapped_types(filename, sip, entry):
+def module_fix_mapped_types(filename, sip, rule):
     #
     # SIP cannot handle duplicate %MappedTypes.
     #
-    rule_helpers.modulecode_delete(filename, sip, entry, "QList<QUrl>", "QExplicitlySharedDataPointer<KService>",
+    rule_helpers.modulecode_delete(filename, sip, rule, "QList<QUrl>", "QExplicitlySharedDataPointer<KService>",
                                    "QList<KServiceAction>", "QList<QAction *>", "QList<QSslCertificate>",
                                    "QList<QExplicitlySharedDataPointer<KService> >", "QList<QModelIndex>")
+    rule_helpers.module_add_imports(filename, sip, rule, "KIOCore/kio/kiomod.sip")
+    rule_helpers.module_add_classes(filename, sip, rule, "KIO::Connection", "KIO::ClipboardUpdater",
+                                    "KSslCertificateBoxPrivate", "KCModule")
+
+
+def module_fix_mapped_types_kio(filename, sip, rule):
+    rule_helpers.module_add_imports(filename, sip, rule, "KIOCore/kio/kiomod.sip")
+    rule_helpers.module_add_classes(filename, sip, rule, "KIO::Connection", "KService", "KIO::ClipboardUpdater",
+                                    "KPixmapSequence")
 
 
 def modulecode():
     return {
         "KIOWidgets/KIOWidgetsmod.sip": {
             "code": module_fix_mapped_types,
+        },
+        "KIOWidgets/KIO/KIOmod.sip": {
+            "code": module_fix_mapped_types_kio,
         },
     }
