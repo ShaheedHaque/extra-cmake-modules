b275192a2c5b1446563d9810ed89ed5da21d4a42
Shaheed Haque <srhaque@theiet.org>
2017-09-05 00:08:45 +0100

    Get KrossUi compiling.

--- find-modules/module_generation/PyKF5/Kross.py
+++ find-modules/module_generation/PyKF5/Kross.py
@@ -21,20 +21,53 @@ SIP binding customisation for PyKF5.Kross. This modules describes:
 
     * Supplementary SIP file generator rules.
 """
-
 import rule_helpers
 
 
-def module_fix_mapped_types(filename, sip, entry):
+def container_fixup(container, sip, rule):
+    rule_helpers.container_discard_QSharedData_base(container, sip, rule)
+    rule_helpers.container_make_unassignable(container, sip, rule)
+
+
+def module_fix_mapped_types_core(filename, sip, rule):
     #
     # SIP cannot handle duplicate %MappedTypes.
     #
-    rule_helpers.modulecode_make_local(filename, sip, entry, "QList<QVariant>", "QMap<QString, QVariant>")
+    rule_helpers.modulecode_make_local(filename, sip, rule, "QList<QVariant>", "QMap<QString, QVariant>")
+    rule_helpers.module_add_imports(filename, sip, rule, "SonnetCore/Sonnet/Sonnetmod.sip")
+    rule_helpers.module_add_classes(filename, sip, rule, "QScriptable")
+
+
+def module_fix_mapped_types_ui(filename, sip, rule):
+    rule_helpers.module_add_imports(filename, sip, rule, "KIOCore/kio/kiomod.sip")
+    rule_helpers.module_add_classes(filename, sip, rule, "KXmlGuiWindow", "QScriptable", "KIO::Connection",
+                                    "KIO::ClipboardUpdater", "KIconLoader")
+
+
+def container_rules():
+    return [
+        #
+        # SIP does not seem to be able to handle these type specialization, but we can live without them?
+        #
+        ["Kross", "MetaTypeVariant", "", ".*", ".*", rule_helpers.container_discard],
+        ["Kross", "Object", ".*", ".*", ".*", container_fixup],
+        ["Kross", "InterpreterInfo", ".*", ".*", ".*", rule_helpers.container_fake_derived_class],
+    ]
+
+
+def function_rules():
+    return [
+        ["Kross::Manager", "registerMetaTypeHandler", ".*", ".*", ".*", rule_helpers.function_discard],
+        ["Kross::MetaTypeHandler", "MetaTypeHandler", ".*", ".*", ".*func", rule_helpers.function_discard],
+    ]
 
 
 def modulecode():
     return {
         "KrossCore/Kross/Core/Coremod.sip": {
-            "code": module_fix_mapped_types,
+            "code": module_fix_mapped_types_core,
+        },
+        "KrossUi/Kross/Ui/Uimod.sip": {
+            "code": module_fix_mapped_types_ui,
         },
     }
