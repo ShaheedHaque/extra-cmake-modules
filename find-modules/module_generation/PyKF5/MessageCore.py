58ea66f906a416ae4dd06d62b90bc50bc4a1acdf
Shaheed Haque <srhaque@theiet.org>
2017-09-05 11:33:35 +0100

    Get MessageCore compiling.

--- find-modules/module_generation/PyKF5/MessageCore.py
+++ find-modules/module_generation/PyKF5/MessageCore.py
@@ -21,19 +21,34 @@ SIP binding customisation for PyKF5.MessageCore. This modules describes:
 
     * Supplementary SIP file generator rules.
 """
-
 import rule_helpers
 
 
-def module_fix_mapped_types(filename, sip, entry):
+def module_fix_mapped_types(filename, sip, rule):
     #
     # SIP cannot handle duplicate %MappedTypes.
     #
-    rule_helpers.modulecode_delete(filename, sip, entry, "QSharedPointer<KMime::Message>", "QList<long long>",
+    rule_helpers.modulecode_delete(filename, sip, rule, "QSharedPointer<KMime::Message>", "QList<long long>",
                                    "QVector<KMime::Types::Mailbox>", "QVector<KMime::Types::Address>")
-    rule_helpers.module_add_classes(filename, sip, entry, "MessageCore::MessageCoreSettingsBase",
-                                  "Akonadi::Protocol::Command", "Akonadi::ServerManagerPrivate")
-    rule_helpers.modulecode_make_local(filename, sip, entry, "QList<QUrl>")
+
+    rule_helpers.module_delete_imports(filename, sip, rule, "Akonadi/KMime/KMimemod.sip")
+    rule_helpers.module_add_classes(filename, sip, rule, "MessageCore::MessageCoreSettingsBase",
+                                    "Akonadi::Protocol::Command", "Akonadi::ServerManagerPrivate",
+                                    "Akonadi::SpecialMailCollectionsPrivate", "KLocalizedString", "KConfigGroup",
+                                    "KCoreConfigSkeleton", "Akonadi::MessageStatus")
+    rule_helpers.modulecode_make_local(filename, sip, rule, "QList<QUrl>")
+
+
+def container_rules():
+    return [
+        ["MessageCore", "MessageCoreSettings", ".*", ".*", ".*", rule_helpers.container_make_uncopyable]
+    ]
+
+
+def parameter_rules():
+    return [
+        ["MessageCore::AttachmentFromUrlUtils", "createAttachmentJob", "parent", ".*", ".*", rule_helpers.noop]
+    ]
 
 
 def typedef_rules():
